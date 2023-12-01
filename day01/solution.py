9c746e840528ec39c0609f1be373d1b0715a99ee
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-01 11:32:45 +0100

    docs

--- day01/solution.py
+++ day01/solution.py
@@ -8,24 +8,16 @@ spicy_number_string = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5'
 def from_line(line):
     # only replace first and last match. lowest find, highest rfind
     lowest_text_match = min(spicy_number_string.items(), key=lambda x: line.find(x[0]) if x[0] in line else sys.maxsize)
-    highest_text_match = max(spicy_number_string.items(), key=lambda x: line.rfind(x[0]))
+    # replace is not meant to exhaust the digit 🤡
+    # if we don't do this magic, oneight would evaluate to 11 when it should be 18
+    line = line.replace(lowest_text_match[0], lowest_text_match[1] + lowest_text_match[0])
 
-    line = line.replace(lowest_text_match[0], lowest_text_match[1] + lowest_text_match[0]) # replace is not meant to exhaust the digit 🤡
+    highest_text_match = max(spicy_number_string.items(), key=lambda x: line.rfind(x[0]))
     line = line.replace(highest_text_match[0], highest_text_match[1])
 
     numbers = [int(x) for x in line if x in number_strings]
     return int(str(numbers[0]) + str(numbers[-1]))
 
 if __name__ == '__main__':
-    # for i in [
-    #     'two1nine',
-    #     'eightwothree',
-    #     'abcone2threexyz',
-    #     'xtwone3four',
-    #     '4nineeightseven2',
-    #     'zoneight234',
-    #     '7pqrstsixteen'
-    # ]:
-    #     print(from_line(i))
     with open('input.txt') as problem:
         print(sum([from_line(line) for line in problem.readlines()]))
