1a87583ba85ff500174cf8fce193717493ffbcd3
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-01 13:53:35 +0100

    spicier python

--- day01/solution.py
+++ day01/solution.py
@@ -1,22 +1,26 @@
 import sys
 
 # part 1 legacy
-number_strings = [str(x) for x in range(0,10)]
-spicy_number_string = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+number_strings = [str(x) for x in range(1, 10)]
+spicy_number_strings = dict(
+    zip(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], number_strings))
+
 
 # part 2
 def from_line(line):
     # only replace first and last match. lowest find, highest rfind
-    lowest_text_match = min(spicy_number_string.items(), key=lambda x: line.find(x[0]) if x[0] in line else sys.maxsize)
+    lowest_text_match = min(spicy_number_strings.items(),
+                            key=lambda x: line.find(x[0]) if x[0] in line else sys.maxsize)
     # replace is not meant to exhaust the digit 🤡
     # if we don't do this magic, oneight would evaluate to 11 when it should be 18
     line = line.replace(lowest_text_match[0], lowest_text_match[1] + lowest_text_match[0])
 
-    highest_text_match = max(spicy_number_string.items(), key=lambda x: line.rfind(x[0]))
+    highest_text_match = max(spicy_number_strings.items(), key=lambda x: line.rfind(x[0]))
     line = line.replace(highest_text_match[0], highest_text_match[1])
 
     numbers = [int(x) for x in line if x in number_strings]
     return int(str(numbers[0]) + str(numbers[-1]))
+
 
 if __name__ == '__main__':
     with open('input.txt') as problem:
