14cbd7e240f921d950e73db804f11fb51d8bf45b
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-02 07:42:18 +0100

    minor cleanup

--- day02/solution.py
+++ day02/solution.py
@@ -1,22 +1,6 @@
 import re
 
-
-def part_2(problem):
-    sum_ok = 0
-
-    for line in problem.readlines():
-
-        limits = {'red': 0, 'green': 0, 'blue': 0}
-        pattern = re.compile(r'\b(\d+)\s+(red|blue|green)\b')
-        matches = pattern.findall(line)
-
-        for number, color in matches:
-            print(limits)
-            print(f'{color}, {number}')
-            limits[color] = max(limits[color], int(number))
-
-        sum_ok += limits['red'] * limits['green'] * limits['blue']
-    print(sum_ok)  # 1683 bad
+color_count_pattern = re.compile(r'\b(\d+)\s+(red|blue|green)\b')
 
 
 # 12 red cubes, 13 green cubes, and 14 blue cubes
@@ -26,23 +10,33 @@ def part_1(problem):
     limits = {'red': 12, 'green': 13, 'blue': 14}
 
     for line in problem.readlines():
-        lr = line.split(':')
-        game_nr = lr[0].split(' ')[1]
-
-        pattern = re.compile(r'\b(\d+)\s+(red|blue|green)\b')
-        matches = pattern.findall(line)
-
+        matches = color_count_pattern.findall(line)
         ok = True
         for number, color in matches:
-            # print(f'number {number}, color {color}')
             ok = ok and limits[color] >= int(number)
-            # print(f'limit[{color}]: {limits[color]}, {number}')
 
         if ok:
-            sum_ok += int(game_nr)
-    print(sum_ok)  # 1683 bad
+            sum_ok += int(line.split(':')[0].split(' ')[1])  # peak engineering
+    print(sum_ok)
+
+
+def part_2(problem):
+    sum_products = 0
+
+    for line in problem.readlines():
+
+        limits = {'red': 0, 'green': 0, 'blue': 0}
+        matches = color_count_pattern.findall(line)
+
+        for number, color in matches:
+            limits[color] = max(limits[color], int(number))
+
+        sum_products += limits['red'] * limits['green'] * limits['blue']
+    print(sum_products)
 
 
 if __name__ == '__main__':
+    with open('input.txt') as problem:
+        part_1(problem)
     with open('input.txt') as problem:
         part_2(problem)
