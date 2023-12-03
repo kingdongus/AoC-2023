f9eaf8d1b6a4c644a8e33ff973b4cd23c1a0bca0
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-03 07:49:39 +0100

    - extracted utility functions and var in anticipation - minor cleanup

--- day03/solution.py
+++ day03/solution.py
@@ -1,104 +1,75 @@
-directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-
-def read_file_into_2d_array(file_path):
-    try:
-        # Open the file for reading
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-            two_d_array = []
-
-            for line in lines:
-                values = list(line)
-                two_d_array.append(values)
-
-            return two_d_array
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        return None
+from toolbox.toolbox import read_file_into_2d_array, directions, number_strings, in_range
 
 
 def part_1(problem):
-    as_array = read_file_into_2d_array('input.txt')
     numbers = [str(i) for i in range(10)]
 
     gear_sum = 0
 
-    for y in range(len(as_array)):
-        for x in range(len(as_array[y])):
-            if as_array[y][x] not in numbers and as_array[y][x] not in ['.', '\n']:
+    for y in range(len(problem)):
+        for x in range(len(problem[y])):
+            if problem[y][x] not in numbers and problem[y][x] not in ['.', '\n']:
                 # check in all directions to see if we find numbers
                 for direction in directions:
                     direction_new_x = x + direction[0]
                     direction_new_y = y + direction[1]
-                    if direction_new_x > -1 and direction_new_x < len(
-                            as_array[y]) and direction_new_y > -1 and direction_new_y < len(as_array) and \
-                            as_array[direction_new_y][direction_new_x] in numbers:
+                    if in_range(direction_new_x, problem[y]) and in_range(direction_new_y, problem) and \
+                            problem[direction_new_y][direction_new_x] in numbers:
                         # expand and erase number
                         ny = direction_new_y
                         nx = direction_new_x
 
                         # move left until you don't find a number anymore
-                        while nx > -1 and as_array[ny][nx - 1] in numbers:
+                        while in_range(nx, problem[y]) and problem[ny][nx - 1] in numbers:
                             nx -= 1
                         # move right until you don't find a number anymore
-                        num = as_array[ny][nx]
-                        as_array[ny][nx] = '.'
+                        num = problem[ny][nx]
+                        problem[ny][nx] = '.'
 
-                        while nx < len(as_array[y]) and as_array[ny][nx + 1] in numbers:
+                        while in_range(nx, problem[y]) and problem[ny][nx + 1] in numbers:
                             nx += 1
-                            num += as_array[ny][nx]
-                            as_array[ny][nx] = '.'
-                        print(num)
+                            num += problem[ny][nx]
+                            problem[ny][nx] = '.'
 
                         gear_sum += int(num)
-    print(gear_sum)
+    return gear_sum
 
 
 def part_2(problem):
-    as_array = read_file_into_2d_array('input.txt')
-    numbers = [str(i) for i in range(10)]
-
     gear_ratio_product = 0
 
-    for y in range(len(as_array)):
-        for x in range(len(as_array[y])):
-            if as_array[y][x] == '*':
+    for y in range(len(problem)):
+        for x in range(len(problem[y])):
+            if problem[y][x] == '*':
                 # check in all directions to see if we find numbers
                 surrounding_numbers = []
                 for direction in directions:
                     direction_new_x = x + direction[0]
                     direction_new_y = y + direction[1]
-                    if direction_new_x > -1 and direction_new_x < len(
-                            as_array[y]) and direction_new_y > -1 and direction_new_y < len(as_array) and \
-                            as_array[direction_new_y][direction_new_x] in numbers:
+                    if in_range(direction_new_x, problem[y]) and in_range(direction_new_y, problem) and \
+                            problem[direction_new_y][direction_new_x] in number_strings:
                         # expand number
                         ny = direction_new_y
                         nx = direction_new_x
 
                         # move left until you don't find a number anymore
-                        while nx > -1 and as_array[ny][nx - 1] in numbers:
+                        while in_range(nx, problem[y]) and problem[ny][nx - 1] in number_strings:
                             nx -= 1
                         # move right until you don't find a number anymore
-                        num = as_array[ny][nx]
-                        as_array[ny][nx] = '.'
+                        num = problem[ny][nx]
+                        problem[ny][nx] = '.'
 
-                        while nx < len(as_array[y]) and as_array[ny][nx + 1] in numbers:
+                        while in_range(nx, problem[y]) and problem[ny][nx + 1] in number_strings:
                             nx += 1
-                            num += as_array[ny][nx]
-                            as_array[ny][nx] = '.'
-                        print(num)
+                            num += problem[ny][nx]
+                            problem[ny][nx] = '.'
                         surrounding_numbers.append(num)
 
                 if len(surrounding_numbers) == 2:
-                    print(surrounding_numbers)
                     gear_ratio_product += int(surrounding_numbers[0]) * int(surrounding_numbers[1])
-    print(gear_ratio_product)
+    return gear_ratio_product
 
 
 if __name__ == '__main__':
-    with open('input.txt') as problem:
-        part_1(problem)
-    with open('input.txt') as problem:
-        part_2(problem)
+    print(part_1(read_file_into_2d_array('input.txt')))
+    print(part_2(read_file_into_2d_array('input.txt')))
---
+++ toolbox/toolbox.py
@@ -0,0 +1,17 @@
+number_strings = [str(i) for i in range(10)]
+directions = [(x, y) for x in [-1, 0, 1] for y in [-1, 0, 1] if not (x == 0 and y == 0)]
+
+input_file_name = 'input.txt'
+
+
+def read_file_into_2d_array(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return [list(line) for line in file.readlines()]
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+
+
+def in_range(index, list):
+    return -1 < index < len(list)
