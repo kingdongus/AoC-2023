eb35b4c073de78b605db9e5138f0dc47bd643bab
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-08 06:49:29 +0100

    cleanup

---
+++ day08/solution.py
@@ -0,0 +1,60 @@
+import re
+
+from toolbox.toolbox import input_file_name, lcmm
+
+pattern = re.compile(r'(\w+) = \((\w+), (\w+)\)')
+
+
+def extract_node(input_string):
+    matches = pattern.findall(input_string)
+    if matches:
+        return matches[0][0], (matches[0][1], matches[0][2])
+    else:
+        return None
+
+
+def parse_graph(problem):
+    instructions = list(problem.readline().replace('\n', ''))
+    problem.readline()  # skip empy line 🤡
+    network = {}
+    for line in problem.readlines():
+        node, rl = extract_node(line)
+        network[node] = rl
+    return instructions, network
+
+
+def find_path_for_node(current_node, instructions, network, end_condition):
+    steps = 0
+    # while not current_node == 'ZZZ':
+    while not end_condition(current_node):
+        instruction = instructions[steps % len(instructions)]
+        next_targets = network[current_node]
+        next_node = next_targets[0] if instruction == 'L' else next_targets[1]
+        steps += 1
+        current_node = next_node
+    return steps
+
+
+def all_end_with_z(strings):
+    return len(strings) == len([s for s in strings if s.endswith('Z')])
+
+
+def part_1(problem):
+    instructions, network = parse_graph(problem)
+    current_node = 'AAA'
+    return find_path_for_node(current_node, instructions, network, lambda x: x == 'ZZZ')
+
+
+def part_2(problem):
+    instructions, network = parse_graph(problem)
+    current_nodes = [node for node in network.keys() if node.endswith('A')]
+    individual_solutions = [find_path_for_node(node, instructions, network, lambda x: x.endswith('Z')) for node in
+                            current_nodes]
+    return lcmm(*individual_solutions)
+
+
+if __name__ == '__main__':
+    with open(input_file_name) as problem:
+        print(part_1(problem))
+    with open(input_file_name) as problem:
+        print(part_2(problem))
--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 number_strings = [str(i) for i in range(10)]
 directions_2d = [(x, y) for x in [-1, 0, 1] for y in [-1, 0, 1] if not (x == 0 and y == 0)]
 
@@ -15,3 +17,20 @@ def read_file_into_2d_array(file_path):
 
 def in_range(index, array):
     return -1 < index < len(array)
+
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+def lcmm(*args):
+    """Return lcm of args."""
+    return reduce(lcm, args)
