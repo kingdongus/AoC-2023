df2aa723ccc7ce95a4b0f9ad2396ba7d793e9e04
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-17 11:06:47 +0100

    cleanup

--- day17/solution.py
+++ day17/solution.py
@@ -11,64 +11,39 @@ opposites = {
 }
 
 
-def traverse(city):
-    seen = set()
-    work = [(0, (0, 0), direction_east, 0), (0, (0, 0), direction_south, 0)]
-
-    while work:
-        heat, position, direction, direction_count = heappop(work)
-        if (position, direction, direction_count) in seen:
-            continue
-        seen.add((position, direction, direction_count))
-
-        new_row, new_col = position[0] + direction[0], position[1] + direction[1]
-        if not in_range(new_col, city) or not in_range(new_row, city[0]):
-            continue
-        new_heat = heat + city[new_col][new_row]
-        if direction_count < 4:
-            if new_col == len(city) - 1 and new_row == len(city[0]) - 1:
-                return new_heat
-        for d in directions_2d_4:
-            if d == opposites[direction]:
-                continue
-            new_direction_count = direction_count + 1 if d == direction else 1
-            if new_direction_count > 3:
-                continue
-            heappush(work, (new_heat, (new_row, new_col), d, new_direction_count))
-
-
-def traverse_2(city):
+def traverse_2(city, min_step, max_step):
     seen = set()
     work = [(0, (0, 0), direction_east, 1), (0, (0, 0), direction_south, 1)]
 
     while work:
-        heat, position, direction, direction_count = heappop(work)
-        if (position, direction, direction_count) in seen:
+        heat_loss, position, direction, consecutive_steps = heappop(work)
+        if (position, direction, consecutive_steps) in seen:
             continue
-        seen.add((position, direction, direction_count))
+        seen.add((position, direction, consecutive_steps))
 
         new_row, new_col = position[0] + direction[0], position[1] + direction[1]
         if not in_range(new_col, city) or not in_range(new_row, city[0]):
             continue
-        new_heat = heat + city[new_col][new_row]
-        if 4 <= direction_count <= 10:
+        new_heat_loss = heat_loss + city[new_col][new_row]
+        if min_step <= consecutive_steps <= max_step:
             if new_col == len(city) - 1 and new_row == len(city[0]) - 1:
-                return new_heat
-        for d in directions_2d_4:
-            if d == opposites[direction]:
+                return new_heat_loss
+        for new_direction in directions_2d_4:
+            if new_direction == opposites[direction]:
                 continue
-            new_direction_count = direction_count + 1 if d == direction else 1
-            if (direction != d and direction_count < 4) or new_direction_count > 10:
+            new_direction_count = consecutive_steps + 1 if new_direction == direction else 1
+            if (
+                    direction != new_direction and consecutive_steps < min_step) or new_direction_count > max_step:  # need to move at least min_step consecutive_steps in a straight line before one can turn
                 continue
-            heappush(work, (new_heat, (new_row, new_col), d, new_direction_count))
+            heappush(work, (new_heat_loss, (new_row, new_col), new_direction, new_direction_count))
 
 
 def part_1(problem):
-    return traverse(problem)
+    return traverse_2(problem, 1, 3)
 
 
 def part_2(problem):
-    return traverse_2(problem)
+    return traverse_2(problem, 4, 10)
 
 
 if __name__ == '__main__':
@@ -77,4 +52,4 @@ if __name__ == '__main__':
         for j in range(len(problem[0])):
             problem[i][j] = int(problem[i][j])
     print(part_1(problem))
-    print(part_2(problem))  # 627 too low, 876 too low
+    print(part_2(problem))
