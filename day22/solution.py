5053baa270e61cfdb99445360c7541ddfc16093d
sascha <chunkyboy.extrachunk@gmail.com>
2023-12-22 21:47:22 +0100

    solution day 22 part 2

--- day22/solution.py
+++ day22/solution.py
@@ -8,7 +8,6 @@ def part_1(problem):
 
     # 'ground:' z = 0, brick min z = 1
     bricks.sort(key=lambda b: b[0][2])
-    print(bricks)
 
     highest = defaultdict(lambda: (0, -1))  # for each column in 3d, track the highest occupied block
 
@@ -49,8 +48,56 @@ def parse_bricks(problem):
     return bricks
 
 
+def count_affected_for_idx(idx, graph):
+    incoming = [0] * len(graph)
+    for i in range(len(incoming)):
+        for j in graph[i]:
+            incoming[j] += 1
+    work = [idx]
+    num_affected_bricks = 0
+    while work:
+        num_affected_bricks += 1
+        for i in graph[work.pop()]:
+            incoming[i] -= 1
+            if incoming[i] == 0:
+                work.append(i)
+    return num_affected_bricks - 1
+
+
 def part_2(problem):
-    pass
+    bricks = parse_bricks(problem)
+    # 'ground:' z = 0, brick min z = 1
+    bricks.sort(key=lambda b: b[0][2])
+
+    highest = defaultdict(lambda: (0, -1))  # for each column in 3d, track the highest occupied block
+
+    graph = [[] for _ in range(len(bricks))]
+
+    for idx, brick in enumerate(bricks):
+        max_height = -1
+        support_set = set()
+        for x in range(brick[0][0], brick[1][0] + 1):
+            for y in range(brick[0][1], brick[1][1] + 1):
+                if highest[x, y][0] + 1 > max_height:
+                    max_height = highest[x, y][0] + 1
+                    support_set = {highest[x, y][1]}
+                elif highest[x, y][0] + 1 == max_height:
+                    support_set.add(highest[x, y][1])
+        for x in support_set:
+            if x != -1:
+                graph[x].append(idx)
+
+        # move brick down
+        delta_z = brick[0][2] - max_height
+        brick[0][2] -= delta_z
+        brick[1][2] -= delta_z
+
+        # update highest with new brick
+        for x in range(brick[0][0], brick[1][0] + 1):
+            for y in range(brick[0][1], brick[1][1] + 1):
+                highest[x, y] = (brick[1][2], idx)
+
+    return sum(count_affected_for_idx(x, graph) for x in range(len(graph)))
 
 
 if __name__ == '__main__':
